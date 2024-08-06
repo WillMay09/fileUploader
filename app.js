b0298a966bc2b831156b4666d6edde4aed08ff4f
William <wmayhood@outlook.com>
2024-08-06 10:37:51 -0400

    Created a basic express app with the correct local port and required packages installed

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+
+//Routes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//basic server setup
+app.listen(PORT,() =>{`App is listening on ${PORT}`});
