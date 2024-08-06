dd9ee3e090afe6d793dda1233149d2cbc34c750e
William <wmayhood@outlook.com>
2024-08-06 11:00:17 -0400

    Created the index, login and signup get routes
    These routes will serve as the foundation of the website to build off of

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-
-
+require('dotenv').config
+const indexRouter = require("./routes/indexRouter");
+const logInRouter = require("./routes/logInRouter");
+const signUpRouter = require("./routes/signUpRouter");
 
 //Routes
-
-
+app.use('/', indexRouter);
+app.use('/login', logInRouter);
+app.use('/signUp', signUpRouter);
 
 
 
@@ -22,4 +24,4 @@ const PORT = process.env.PORT || 3000;
 
 
 //basic server setup
-app.listen(PORT,() =>{`App is listening on ${PORT}`});
+app.listen(PORT,() =>{console.log(`App is listening on ${PORT}`);});
---
+++ routes/indexRouter.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+
+router.get('/', (req, res)=>{
+
+    res.send("NOT YET IMPLEMENTED");
+
+});
+
+
+module.exports = router;
