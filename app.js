600c035bf0565c77ce94b764243c93304c2fd010
William <wmayhood@outlook.com>
2024-08-07 15:11:31 -0400

    Created a connection pool and connected to postgredb uploader
    This is a local db for production purposes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const { Pool } = require("pg");
 const app = express();
 const PORT = process.env.PORT || 3000;
 require('dotenv').config
@@ -13,6 +14,22 @@ const signUpRouter = require("./routes/signUpRouter");
 app.set('views',path.join(__dirname,'views'));
 app.set("view engine", 'ejs');
 app.use(express.urlencoded({extended: true}));
+
+
+//database connection
+const pool = new Pool({
+
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT || 2000,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+
+
+});
 
 
 //Routes
